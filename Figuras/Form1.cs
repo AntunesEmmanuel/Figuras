2dedf6148564d9678854768b7a6dc1f7d813e955
Emmanuel Antunes <antunesm.emmanuel@gmail.com>
2016-04-16 19:08:28 -0700

    Figuras
    Version 1.0

---
+++ Figuras/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Figuras
+{
+    public partial class Form1 : Form
+    {
+        List<Figura> figuras;
+        public Form1()
+        {
+            figuras = new List<Figura>();
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (MouseButtons.Right == e.Button)
+            {
+                Circulo c = new Circulo(e.X, e.Y);
+                c.Dibuja(this);
+                figuras.Add(c);
+            }
+            else if (MouseButtons.Left == e.Button)
+            {
+                Rectangulo r = new Rectangulo(e.X, e.Y);
+                r.Dibuja(this);
+                figuras.Add(r);
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(Figura f in figuras)
+            {
+              f.Dibuja(this);
+            }
+        }
+
+    }
+}
